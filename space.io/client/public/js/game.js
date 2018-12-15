3461d5ad9d5283839df2e9d3dcb83508180e49d7
grbcp5 <grbcp5@mst.edu>
2018-12-14 20:26:42 -0600

    Player changes direction based on mouse position

--- space.io/client/public/js/game.js
+++ space.io/client/public/js/game.js
@@ -4,18 +4,132 @@
 
 const socket = io();
 
+const eventType = {
+  connection: 'connection',
+  yourSocketId: 'yourSocketId',
+  alert: 'alert',
+  consoleLog: 'consoleLog',
+  newPlayer: 'newPlayer',
+  updatePlayerDirection: 'updatePlayerDirection',
+  newGameFrame: 'newGameFrame'
+};
 
-socket.on( 'alert', function ( data ) {
+let canvas = document.getElementById( 'canvas' );
+canvas.width = 600;
+canvas.height = 600;
+
+let socketId = "";
+let player = {
+  displayName: "",
+  pos: {
+    x: -1,
+    y: -1
+  },
+  direction: Math.PI / 2
+};
+let newPlayerDirection = Math.PI / 2;
+
+socket.on( eventType.alert, function ( data ) {
   "use strict";
   
   alert( data );
   
 } );
 
-
-socket.on( 'console.log', function( data ) {
+socket.on( eventType.consoleLog, function ( data ) {
   "use strict";
   
   console.log( data );
+  
+} );
+
+socket.on( eventType.yourSocketId, function ( data ) {
+  socketId = data;
+} );
+
+player.name = prompt( "Enter a user name" );
+socket.emit( eventType.newPlayer, player );
+
+
+document.addEventListener( 'keydown', function ( event ) {
+  
+  switch ( event.keyCode ) {
+    case 65: // A
+      newPlayerDirection = Math.PI;
+      break;
+    case 87: // W
+      newPlayerDirection = Math.PI / 2;
+      break;
+    case 68: // D
+      newPlayerDirection = 0;
+      break;
+    case 83: // S
+      newPlayerDirection = 3 * Math.PI / 2;
+      break;
+  }
+} );
+
+
+function getMousePos( canvas, evt ) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+canvas.addEventListener( 'mousemove', function ( evt ) {
+  let mousePos = getMousePos( canvas, evt );
+  
+  newPlayerDirection = Math.atan( ( mousePos.y - player.pos.y ) / ( mousePos.x - player.pos.x ) );
+  if( mousePos.x < player.pos.x ) {
+    newPlayerDirection += Math.PI;
+  }
+} );
+
+function draw() {
+  
+  if ( !canvas.getContext ) {
+    return;
+  }
+  
+  let ctx = canvas.getContext( '2d' );
+  
+  ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
+  drawPlayer( ctx );
+}
+
+
+function drawPlayer( ctx ) {
+  
+  ctx.beginPath();
+  ctx.moveTo( player.pos.x, player.pos.y );
+  ctx.lineTo(
+    player.pos.x + Math.round( -20 * Math.cos( player.direction ) ),
+    player.pos.y + Math.round( -20 * Math.sin( player.direction ) )
+  );
+  ctx.stroke();
+}
+
+
+socket.on( eventType.newGameFrame, function ( data ) {
+  
+  if ( !socketId ) {
+    return
+  }
+  
+  let me = data.players[ socketId ];
+  if ( !me ) {
+    console.error( "This socket is not a player" );
+    return
+  }
+  
+  player = me;
+  
+  setTimeout( function () {
+    draw();
+  }, 1 );
+  
+  socket.emit( eventType.updatePlayerDirection, newPlayerDirection );
   
 } );
--- space.io/server/socket.js
+++ space.io/server/socket.js
@@ -4,6 +4,22 @@
 
 const socketIO = require( 'socket.io' );
 
+const eventType = {
+  connection: 'connection',
+  yourSocketId: 'yourSocketId',
+  alert: 'alert',
+  consoleLog: 'consoleLog',
+  newPlayer: 'newPlayer',
+  updatePlayerDirection: 'updatePlayerDirection',
+  newGameFrame: 'newGameFrame'
+};
+
+
+let gameState = {
+  gameFrameCount: 0,
+  players: {}
+};
+
 
 module.exports.init = function ( server ) {
   "use strict";
@@ -11,11 +27,62 @@ module.exports.init = function ( server ) {
   const io = socketIO( server );
   
   /* When connected to client */
-  io.on( 'connection', function ( socket ) {
+  io.on( eventType.connection, function ( socket ) {
     "use strict";
     
-    io.sockets.emit( 'console.log', 'Hello, world!' );
+    /* When new player event is received */
+    socket.on( eventType.newPlayer, function ( data ) {
+      gameState.players[ socket.id ] = data;
+      gameState.players[ socket.id ].pos.x = 300;
+      gameState.players[ socket.id ].pos.y = 300;
+      gameState.players[ socket.id ].direction = 90;
+  
+      socket.emit( eventType.yourSocketId, socket.id );
+    } );
+    
+    
+    /* When movement event is received */
+    socket.on( eventType.updatePlayerDirection, function ( data ) {
+      
+      let player = gameState.players[ socket.id ];
+      
+      if( !player ) {
+        console.error( "Unrecognized player" );
+        return;
+      }
+      
+      player.direction = data;
+      
+    } );
     
   } );
   
+  
+  setInterval( function () {
+
+    gameState.gameFrameCount += 1;
+
+    for ( let playerId in gameState.players ) {
+
+      if( !gameState.players.hasOwnProperty( playerId) ) {
+        continue;
+      }
+
+      // if ( gameState.players[ playerId ].direction === 0 ) {
+      //   gameState.players[ playerId ].pos.x += 1;
+      // }
+      // if ( gameState.players[ playerId ].direction === 90 ) {
+      //   gameState.players[ playerId ].pos.y += 1;
+      // }
+      // if ( gameState.players[ playerId ].direction === 180 ) {
+      //   gameState.players[ playerId ].pos.x -= 1;
+      // }
+      // if ( gameState.players[ playerId ].direction === 270 ) {
+      //   gameState.players[ playerId ].pos.y -= 1;
+      // }
+    }
+
+    io.sockets.emit( eventType.newGameFrame, gameState );
+
+  }, 1000 / 30 );
 };
